6ed81173a41fd6245db0c9aa36dcac4632c56ec9
MILU-YOU <1359366165@qq.com>
2022-09-07 14:48:07 +0800

    加入redis，缓存验证码和菜品，并且使用Spring

--- src/main/java/com/itheima/reggie/controller/OrderController.java
+++ src/main/java/com/itheima/reggie/controller/OrderController.java
@@ -4,7 +4,6 @@ import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.BaseContext;
 import com.itheima.reggie.common.R;
-import com.itheima.reggie.entity.Employee;
 import com.itheima.reggie.entity.Orders;
 import com.itheima.reggie.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
